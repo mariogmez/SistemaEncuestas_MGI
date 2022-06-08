f3bbb12be6726238a04d726025e83870e7de97cd
mariogmez <mariogomex199921@gmail.com>
2022-06-08 11:47:03 +0200

    CMT 11:46 08/06 INIT

---
+++ app/src/main/java/com/example/sistemaencuestas_mgi/Usuario/MenuUsuarioActivity.kt
@@ -0,0 +1,12 @@
+package com.example.sistemaencuestas_mgi.Usuario
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.sistemaencuestas_mgi.R
+
+class MenuUsuarioActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_menu_usuario)
+    }
+}
